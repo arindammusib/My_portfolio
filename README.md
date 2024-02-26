28d82b002153042b1b0bcf5bb42e6a348ee78c2e
Arindam <amusib32@gmail.com>
2024-02-26 20:43:16 +0530

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio webpage
+ - Using Html, Css ,javascript
---
+++ index.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Full Stack Developer",
+        "Web Developer",
+        "Backend Developer",
+        "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
